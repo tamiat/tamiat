9692c5352a5566bfe5e57b7697e0a0a0ad9b0d42
Abdelaziz18003 <m.abdelaziz1993@gmail.com>
2017-07-30 17:12:41 +0100

    add the admin component and make routing works

---
+++ src/Admin.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>this is the admin section</h1>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import Home from '../Home'
+import Admin from '../Admin'
 
 Vue.use(Router)
 
@@ -10,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/admin',
+      name: 'Admin',
+      component: Admin
     }
   ]
 })

917b30ecc80593f1485cf9e8369b242c3c495329
Abdelaziz18003 <m.abdelaziz1993@gmail.com>
2017-07-31 00:06:19 +0100

    move configuration code from Home.vue to config.js

--- src/Home.vue
+++ src/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+
     <header>
       <h2><a href="#">Website Logo</a></h2>
       <nav>
@@ -10,10 +11,11 @@
         <li><a href="#">Contacts</a></li>
       </nav>
     </header>
+
     <section class="hero">
       <div class="background-image" :style="{'background-image': `url(${require('./assets/img/hero.jpg')})`}"></div>
       <h1 v-for="pconf in pconfig">
-        {{pconfig[0].title}}
+         {{pconfig[0].title}} 
       </h1>
       <h3>{{pconfig[0].desc}}</h3>
       <a href="http://tutorialzine.com/2016/06/freebie-landing-page-template-with-flexbox/" class="btn">Download it Here</a>
@@ -23,7 +25,6 @@
       <h3 class="title">Some of our work</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id felis et ipsum bibendum ultrices. Morbi vitae pulvinar velit. Sed aliquam dictum sapien, id sagittis augue malesuada eu.</p>
       <hr>
-
       <ul class="grid">
         <li class="small" :style="{'background-image': `url(${require('./assets/img/coast.jpg')})`}"></li>
         <li class="large" :style="{'background-image': `url(${require('./assets/img/island.jpg')})`}"></li>
@@ -36,7 +37,6 @@
       <h3 class="title">Features and services</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id felis et ipsum bibendum ultrices. Morbi vitae pulvinar velit. Sed aliquam dictum sapien, id sagittis augue malesuada eu.</p>
       <hr>
-
       <ul class="grid">
         <li>
           <i class="fa fa-camera-retro"></i>
@@ -55,6 +55,7 @@
         </li>
       </ul>
     </section>
+    
     <section class="reviews">
       <h3 class="title">What others say:</h3>
 
@@ -67,6 +68,7 @@
       <p class="quote">Donec commodo dolor augue, vitae faucibus tortor tincidunt in. Aliquam vitae leo quis mi pulvinar ornare. Integer eu iaculis metus.</p>
       <p class="author">— Kevin Blake</p>
     </section>
+    
     <section class="contact">
       <h3 class="title">Join our newsletter</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id felis et ipsum bibendum ultrices. Morbi vitae pulvinar velit. Sed aliquam dictum sapien, id sagittis augue malesuada eu.</p>
@@ -77,13 +79,14 @@
         <a href="#" class="btn">Subscribe now</a>
       </form>
     </section>
+
     <footer>
       <ul>
         <li><a href="#"><i class="fa fa-twitter-square"></i></a></li>
         <li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
         <li><a href="#"><i class="fa fa-snapchat-square"></i></a></li>
         <li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
-        <li><a href="#"><i class="fa fa-github-square""></i></a></li>
+        <li><a href="#"><i class="fa fa-github-square"></i></a></li>
       </ul>
       <p>Made by <a href="http://tutorialzine.com/" target="_blank">tutorialzine</a>. images courtesy to <a href="http://unsplash.com/" target="_blank">unsplash</a>.</p>
       <p>No attribution required. you can remove this footer.</p>
@@ -92,21 +95,7 @@
 </template>
 
 <script>
-  import Firebase from 'firebase';
-  let config = {
-    apiKey: "AIzaSyCnxuLX6AgMduDMLtSJVDNJhR8xuMNvs4Y",
-    authDomain: "tamiat-demo.firebaseapp.com",
-    databaseURL: "https://tamiat-demo.firebaseio.com/",
-    projectId: "tamiat-demo",
-    storageBucket: "",
-    messagingSenderId: "188459960333"
-  };
-
-  let app = Firebase.initializeApp(config);
-  let db = app.database();
-  let publicConfig = db.ref('publicconfig');
-
-
+  import publicConfig from './config';
   export default {
     name: 'app',
     firebase: {
@@ -116,9 +105,6 @@
 </script>
 
 <style>
-  #app {
-
-  }
 
   *{
     margin: 0;
---
+++ src/config.js
@@ -0,0 +1,16 @@
+import Firebase from 'firebase';
+
+let config = {
+  apiKey: "AIzaSyCnxuLX6AgMduDMLtSJVDNJhR8xuMNvs4Y",
+  authDomain: "tamiat-demo.firebaseapp.com",
+  databaseURL: "https://tamiat-demo.firebaseio.com/",
+  projectId: "tamiat-demo",
+  storageBucket: "",
+  messagingSenderId: "188459960333"
+};
+
+let app = Firebase.initializeApp(config);
+let db = app.database();
+let publicConfig = db.ref('publicconfig');
+
+export default publicConfig;

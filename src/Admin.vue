005f13564c6354b371a70d942b681eea9c6a0930
Abdelaziz18003 <m.abdelaziz1993@gmail.com>
2017-08-03 22:44:53 +0100

    added sidebar skeleton

--- src/Admin.vue
+++ src/Admin.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <navbar></navbar>
+    <sidebar></sidebar>
   </div>
 </template>
 
 <script>
   import Navbar from './components/Navbar'
+  import Sidebar from './components/Sidebar'
 
   export default {
     components: {
-      'navbar': Navbar
+      'navbar': Navbar,
+      'sidebar': Sidebar
     }
   }
 
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,69 @@
+<template>
+  <aside class="menu app-sidebar">
+
+    <p class="menu-label">
+      General
+    </p>
+
+    <ul class="menu-list">
+      <li v-for="(item, index) in menu">
+        <router-link :to="item.path" v-if="item.path">
+          <span class="icon is-small"><i :class="['fa', item.icon]"></i></span> {{ item.name }}
+        </router-link>
+      </li>
+    </ul>
+
+  </aside>
+</template>
+
+<script>
+  export default {
+
+    data() {
+      return {
+        menu: [
+          {
+            name: 'general',
+            path: '/',
+            icon: 'fa-home'
+          },
+          {
+            name: 'posts',
+            path: '/',
+            icon: 'fa-edit'
+          },
+          {
+            name: 'users',
+            path: '/',
+            icon: 'fa-users'
+          },
+        ]
+      }
+    }
+  }
+
+</script>
+
+<style lang="scss">
+  .app-sidebar {
+    position: fixed;
+    top: 54px;
+    left: 0;
+    bottom: 0;
+    padding: 20px 0 50px;
+    width: 180px;
+    min-width: 45px;
+    max-height: 100vh;
+    height: calc(100% - 50px);
+    z-index: 1024 - 1;
+    background: #FFF;
+    box-shadow: 0 2px 3px rgba(17, 17, 17, 0.1), 0 0 0 1px rgba(17, 17, 17, 0.1);
+    overflow-y: auto;
+    overflow-x: hidden;
+
+    .menu-label {
+      padding-left: 5px;
+    }
+  }
+
+</style>

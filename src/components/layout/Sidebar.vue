a79db25234e237a7b1406fd2152377d805a23323
Abdelaziz18003 <m.abdelaziz1993@gmail.com>
2017-09-04 11:28:17 +0100

    drop the users section from the interface

--- src/components/layout/Sidebar.vue
+++ src/components/layout/Sidebar.vue
@@ -8,7 +8,9 @@
     <ul class="menu-list">
       <li v-for="(item, index) in menu">
         <router-link :to="item.path" v-if="item.path">
-          <span class="icon is-small"><i :class="['fa', item.icon]"></i></span> {{ item.name }}
+          <span class="icon is-small">
+            <i :class="['fa', item.icon]"></i>
+          </span> {{ item.name }}
         </router-link>
       </li>
     </ul>
@@ -17,54 +19,48 @@
 </template>
 
 <script>
-  export default {
+export default {
 
-    data() {
-      return {
-        // this array contains the all the links in the sidebar
-        menu: [
-          {
-            name: 'posts',
-            path: '/admin/posts',
-            icon: 'fa-file-text'
-          },
-          {
-            name: 'users',
-            path: '/admin/users',
-            icon: 'fa-users'
-          },
-          {
-            name: 'Settings',
-            path: '/admin/settings',
-            icon: 'fa-gear'
-          },
-        ]
-      }
+  data() {
+    return {
+      // this array contains the all the links in the sidebar
+      menu: [
+        {
+          name: 'posts',
+          path: '/admin/posts',
+          icon: 'fa-file-text'
+        },
+        {
+          name: 'Settings',
+          path: '/admin/settings',
+          icon: 'fa-gear'
+        },
+      ]
     }
   }
+}
 
 </script>
 
 <style lang="scss">
-  .app-sidebar {
-    position: fixed;
-    top: 54px;
-    left: 0;
-    bottom: 0;
-    padding: 20px 0 50px;
-    width: 180px;
-    min-width: 45px;
-    max-height: 100vh;
-    height: calc(100% - 50px);
-    z-index: 1024 - 1;
-    background: #FFF;
-    box-shadow: 0 2px 3px rgba(17, 17, 17, 0.1), 0 0 0 1px rgba(17, 17, 17, 0.1);
-    overflow-y: auto;
-    overflow-x: hidden;
+.app-sidebar {
+  position: fixed;
+  top: 54px;
+  left: 0;
+  bottom: 0;
+  padding: 20px 0 50px;
+  width: 180px;
+  min-width: 45px;
+  max-height: 100vh;
+  height: calc(100% - 50px);
+  z-index: 1024 - 1;
+  background: #FFF;
+  box-shadow: 0 2px 3px rgba(17, 17, 17, 0.1), 0 0 0 1px rgba(17, 17, 17, 0.1);
+  overflow-y: auto;
+  overflow-x: hidden;
 
-    .menu-label {
-      padding-left: 5px;
-    }
+  .menu-label {
+    padding-left: 5px;
   }
-
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -5,14 +5,11 @@ import Router from 'vue-router';
 import Home from '../Home';
 import Admin from '../Admin';
 import Login from '../components/Login';
-import Signup from '../components/Signup';
 
 // import admin page components
 import Posts from '../components/content/Posts';
 import PostNew from '../components/content/PostNew';
 import PostEdit from '../components/content/PostEdit';
-import Users from '../components/content/Users';
-import UserNew from '../components/content/UserNew';
 import Settings from '../components/content/Settings';
 
 Vue.use(Router);
@@ -31,11 +28,6 @@ export default new Router({
       component: Login
     },
     {
-      path: '/signup',
-      name: 'Signup',
-      component: Signup
-    },
-    {
       path: '/admin',
       name: 'Admin',
       component: Admin,
@@ -51,16 +43,6 @@ export default new Router({
             {
               path: 'edit/:key',
               component: PostEdit
-            }
-          ]
-        },
-        {
-          path: 'users',
-          component: Users,
-          children: [
-            {
-              path: 'new',
-              component: UserNew
             }
           ]
         },

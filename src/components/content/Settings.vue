1791afffbb14895bc3c0a02ddf6a2dfafb41b358
NickolasBoyer <rut.willems@gmail.com>
2017-09-29 18:49:01 +0200

    Fix button spilling

--- src/components/content/Settings.vue
+++ src/components/content/Settings.vue
@@ -21,12 +21,23 @@
               <input type="text" class="input" :name="field.name" :placeholder="settings[field.name]" v-model="field.value">
             </div>
           </div>
-
-          <button type="button" class="button is-info is-pulled-right" @click="saveSettings">
-            Save Settings
-          </button>
+          <!-- Main container -->
+          <nav class="level">
+            <!-- Left side -->
+            <div class="level-left">
+              <!--<div class="level-item"></div>-->
+            </div>
+            <!-- Right side -->
+            <div class="level-right">
+              <div class="level-item">
+                <button type="button" class="button is-info is-pulled-right" @click="saveSettings">
+                  Save Settings
+                </button>
+              </div>
+            </div>
+          </nav>
+          
         </div>
-
       </div>
     </div>
   </div>

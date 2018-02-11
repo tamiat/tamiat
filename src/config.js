0c31964e89057bff1053c10e15cef487851088fb
mornir <jeromepott@hotmail.com>
2018-02-11 12:27:50 +0100

    Custom website title

--- src/config.js
+++ src/config.js
@@ -1,24 +1,25 @@
 /* This is the Firebase configuration file*/
 
-import Firebase from 'firebase';
+import Firebase from 'firebase'
 
-let config = {
-  apiKey: "AIzaSyDN6vYIk-89PI1y1gmI8P-33hvZ8NxT3iI",
-  authDomain: "tamiatcms.firebaseapp.com",
-  databaseURL: "https://tamiatcms.firebaseio.com",
-  projectId: "tamiatcms",
-  storageBucket: "tamiatcms.appspot.com",
-  messagingSenderId: "94963392700"
-};
+const config = {
+  apiKey: 'AIzaSyDN6vYIk-89PI1y1gmI8P-33hvZ8NxT3iI',
+  authDomain: 'tamiatcms.firebaseapp.com',
+  databaseURL: 'https://tamiatcms.firebaseio.com',
+  projectId: 'tamiatcms',
+  storageBucket: 'tamiatcms.appspot.com',
+  messagingSenderId: '94963392700',
+}
 
-let app = Firebase.initializeApp(config);
-let db = app.database();
+const app = Firebase.initializeApp(config)
+const db = app.database()
 
 // create a database references
-const settingsRef = db.ref('settings');
-const pagesRef = db.ref('pages');
-const postsRef = db.ref('posts');
-const usersRef = db.ref('users');
-const mediaRef = db.ref('media');
+const settingsRef = db.ref('settings')
+const defaultsRef = db.ref('defaults')
+const pagesRef = db.ref('pages')
+const postsRef = db.ref('posts')
+const usersRef = db.ref('users')
+const mediaRef = db.ref('media')
 
-export { postsRef, usersRef, settingsRef, pagesRef, mediaRef };
+export { postsRef, usersRef, settingsRef, pagesRef, mediaRef, defaultsRef }

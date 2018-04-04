f47e7b9a2bcaf05e230e611a6f2a6f36bb19d965
Abdelaziz18003 <m.abdelaziz1993@gmail.com>
2018-04-04 17:53:12 +0100

    open external links in the nav menu in new tabs

--- src/app/pages/Home.vue
+++ src/app/pages/Home.vue
@@ -13,12 +13,12 @@
 
       <nav>
         <li v-for="(link, index) in nav" :key="index">
-          <a v-if="link.isAbsolute" :href="link.path">{{link.name}}</a>
+          <a v-if="link.isAbsolute" :href="link.path" target="_blank">{{link.name}}</a>
           <router-link v-else :to="link.path">{{link.name}}</router-link>
 
           <ul v-if="link.children" class="sub-nav">
             <li v-for="(subLink, index) in link.children" :key="index">
-              <a v-if="subLink.isAbsolute" :href="subLink.path">{{subLink.name}}</a>
+              <a v-if="subLink.isAbsolute" :href="subLink.path" target="_blank">{{subLink.name}}</a>
               <router-link v-else :to="subLink.path">{{subLink.name}}</router-link>
             </li>
           </ul>

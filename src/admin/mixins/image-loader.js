aa7a00caae78bbf1ca4fcae34a8f3506973074d5
Abdelaziz Mokhnache <m.abdelaziz1993@gmail.com>
2018-05-02 18:19:59 +0100

    fix image uploading issue #173

--- src/admin/mixins/image-loader.js
+++ src/admin/mixins/image-loader.js
@@ -5,32 +5,23 @@ import { mediaRef } from '@/admin/firebase_config'
 
 export default {
   methods: {
-    // upload image file
-    uploadImage (e) {
+    async uploadImage (e) {
       let file = e.target.files[0]
       let storageRef = firebase.storage().ref('images/' + file.name)
 
-      storageRef.put(file).then((snapshot) => {
-        console.log('Image uploaded')
-        mediaRef.once('value')
-          .then(function (media) {
-            const existingEntry = Object.values(media.val()).find(e => e.path === snapshot.ref.fullPath)
-            if (existingEntry) {
-              console.log(existingEntry)
-              this.insertImage(existingEntry.src)
-            } else {
-              mediaRef.push({
-                src: snapshot.downloadURL,
-                path: snapshot.ref.fullPath,
-                name: snapshot.metadata.name
-              })
-              this.insertImage(snapshot.downloadURL)
-            }
-          })
+      let snapshot = await storageRef.put(file)
+
+      this.createNewMedia(snapshot)
+      this.insertImageIntoDOM(snapshot.downloadURL)
+    },
+    createNewMedia (snapshot) {
+      mediaRef.push({
+        src: snapshot.downloadURL,
+        path: snapshot.ref.fullPath,
+        name: snapshot.metadata.name
       })
     },
-    // insert the uploaded image as a DOM node in the editor
-    insertImage (url) {
+    insertImageIntoDOM (url) {
       let img = document.createElement('img')
       img.src = url
       document.getElementsByClassName('ql-editor')[0].appendChild(img)

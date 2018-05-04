5693dcbd4b4f4df2e0519065e57815dcc2d2e72e
Abdelaziz Mokhnache <m.abdelaziz1993@gmail.com>
2018-05-04 16:05:25 +0100

    add some comments to templates

--- src/app/templates/BlogPost.vue
+++ src/app/templates/BlogPost.vue
@@ -1,4 +1,6 @@
 <template>
+  <!-- this is an example of a dynamic template. -->
+  <!-- Your dynamic content is exposed as 'content' data property in the template. -->
   <div class="template">
     <h1>{{content.title}}</h1>
     <div v-html="content.body"></div>

a224a7d130bfdd3d0f1c6d12d1dc51b6d6b96501
Abdelaziz Mokhnache <m.abdelaziz1993@gmail.com>
2018-05-04 15:13:59 +0100

    fix templates according to new content

--- src/app/templates/LoadContent.js
+++ src/app/templates/LoadContent.js
@@ -6,22 +6,42 @@ export default {
     }
   },
   firebase: {
-    routes: routesRef
+    routes: routesRef,
+    contents: contentsRef
   },
   methods: {
     onLoaded () {
       let filteredRoutes = this.routes.filter((route) => {
         return route.path === this.$route.path
       })
+      let contentType = filteredRoutes[0].contentType
       let contentId = filteredRoutes[0].content || 'none'
-      this.getContentById(contentId)
+      this.content = this.selectContentByTypeAndId(contentType, contentId)
     },
-    getContentById (id) {
-      if (id !== 'none') {
-        contentsRef.child(id).on('value', (snapshot) => {
-          this.content = snapshot.val() || ''
+    selectContentByTypeAndId (type, id) {
+      let contentsByType = this.getContentsByType(type)
+      return contentsByType.filter(content => content['.key'] === id)[0] || {}
+    },
+    getContentsByType (contentType) {
+      let selectedContentsData = []
+      let selectedContents = this.contents.filter(content => {
+        return content.name === contentType
+      })
+      selectedContents.forEach(content => {
+        let contentDataArray = this.convertContentDataToArray(content.data)
+        selectedContentsData = selectedContentsData.concat(contentDataArray || [])
+      })
+      return selectedContentsData
+    },
+    convertContentDataToArray (contentData) {
+      let contentDataArray = []
+      for (let key in contentData) {
+        contentDataArray.push({
+          ...contentData[key],
+          '.key': key
         })
       }
+      return contentDataArray
     }
   }
 }

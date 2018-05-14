a09eeba95c0a83e1d3b975577f69b508acc27ca4
mohamedadel01 <mohamed7adel96@gmail.com>
2018-05-14 15:02:24 +0200

    change the logo

--- src/admin/components/layout/Navbar.vue
+++ src/admin/components/layout/Navbar.vue
@@ -9,9 +9,7 @@
     <!-- the navbar brand -->
     <div class="navbar-brand">
       <router-link class="navbar-item logo" to="/admin">
-        <div>
-         <object type="image/svg+xml" data="/static/img/tamiat-logo-icon-color.svg" style="pointer-events: none;"></object>
-        </div>
+        <img src="/static/img/tamiat-admin-logo-1.png" alt="logo">
       </router-link>
       <div class="navbar-burger burger" data-target="navbar" @click="toggleMenu">
         <span></span>
@@ -103,8 +101,8 @@ $navbarColor: #aaaaaa;
     &:hover {
       color: #4BB885;
     }
-    object{
-      height: 3rem;
+    img{
+      max-height: 3rem;
       margin-left: 1.2rem;
     }
   }

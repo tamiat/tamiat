265167d70a639e306b616093914932eced89cb5a
ilia <ilyakut@gmail.com>
2019-03-04 20:31:35 +0300

    task_6 refactoring

--- src/app/templates/About.vue
+++ src/app/templates/About.vue
@@ -2,10 +2,34 @@
   <!-- this is an example of a static template. No dynamic content here -->
   <div class="template">
     <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, corporis alias ullam fugiat fuga laudantium, provident omnis aperiam voluptatum reprehenderit, accusamus a magni! Velit obcaecati quidem, similique nulla adipisci sapiente!</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, corporis alias ullam fugiat fuga laudantium, provident omnis aperiam voluptatum reprehenderit, accusamus a magni! Velit obcaecati quidem, similique nulla adipisci sapiente!</p>
+    <p>{{ textarea1 }}</p>
+    <p>{{ textarea2 }}</p>
   </div>
 </template>
+
+<script>
+import LoadContent from './LoadContent'
+export default {
+  mixins: [LoadContent],
+  data () {
+    return {}
+  },
+  computed: {
+    currentContent () {
+      return this.content.filter(obj => {
+        return obj['.key'] === this.$route.params.id || obj.slug === this.$route.params.id
+      })[0]
+    },
+    textarea1 () {
+      return this.currentContent['textarea']
+    },
+    textarea2 () {
+      return this.currentContent['2textarea']
+    }
+  },
+  methods: {}
+}
+</script>
 
 <style lang="scss">
 @import '@/app/styles/index.scss';
